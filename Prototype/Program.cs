fddebc5fd54c8d8ea8081909d7ce7acd7dc01fb4
Hasan Hüseyin GÜNEŞ <guneshhg@outlook.com>
2021-10-18 16:57:27 +0300

    prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Person
+    {
+        public  int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();    
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("1997-01-01");
+            p1.Name = "Jack";
+            Person p2 = p1.ShallowCopy();
+
+          
+        }
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -18,13 +18,16 @@ namespace Singleton
             return _instance;
         }
     }
+
+
+
     class Program
     {
         //Singleton is a creational design pattern, which ensures that only one object of its kind exists and provides a single point of access to it for any other code.
         static void Main(string[] args)
         {
            Singleton s1=Singleton.Instance();
-            Singleton s2=Singleton.Instance();
+           Singleton s2=Singleton.Instance();
             if (s1 == s2)
             {
                 Console.WriteLine("Singleton works, both variables contain the same instance.");
